a43f165cab27004931efd896b814d8ff9ef3295e
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-11-19 20:38:56 +0300

    Add symbols to datumaro namespace

--- datumaro/__init__.py
+++ datumaro/__init__.py
@@ -1,3 +1,22 @@
-# Copyright (C) 2019-2020 Intel Corporation
+# Copyright (C) 2019-2021 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
+
+import datumaro.components.errors as errors
+import datumaro.components.project as project
+
+from .components.annotation import (
+    Annotation, AnnotationType, Bbox, Caption, Categories, CompiledMask,
+    Cuboid3d, Label, LabelCategories, Mask, MaskCategories, Points,
+    PointsCategories, Polygon, RleMask,
+)
+from .components.converter import Converter
+from .components.dataset import Dataset, DatasetPatch
+from .components.extractor import (
+    DatasetItem, Extractor, IExtractor, Importer, ItemTransform,
+    SourceExtractor, Transform,
+)
+from .components.launcher import Launcher, ModelTransform
+from .components.media import ByteImage, Image, MediaElement
+from .components.operations import *
+from .components.validator import Validator
